366669fc54540696c0ef2f33d27958aaebb59ebb
Little Hatchling <hatchlingstech@gmail.com>
2026-06-14 13:31:07 +0200

    feat: added diffusion and specular lighting

--- assets/shaders/vertex.glsl
+++ assets/shaders/vertex.glsl
@@ -6,6 +6,8 @@ layout (location = 2) in vec3 colorRGB;
 
 out vec3 colorVS;
 out vec3 normalVS;
+out vec3 lightDirVS;
+out vec3 viewDirVS;
 
 uniform mat4 transformMatrix;
 uniform mat4 viewMatrix;
@@ -16,6 +18,21 @@ uniform vec3 lightColor;
 void main()
 {
     colorVS = colorRGB;
-    normalVS = normal;
-    gl_Position = projectionMatrix * viewMatrix * transformMatrix * vec4(pos, 1.0);
+    
+    // Transform normal to view space
+    vec4 n = vec4(normal, 0.0);
+    mat4 normalMat = transpose(inverse(viewMatrix * transformMatrix));
+    normalVS = (normalMat * n).xyz;
+    
+    // Transform vertex position to view space
+    vec4 P = viewMatrix * transformMatrix * vec4(pos, 1.0);
+    
+    // Transform light position to view space and compute light direction
+    vec4 lp = viewMatrix * vec4(lightPosition, 1.0);
+    lightDirVS = (lp - P).xyz;
+    
+    // View direction (camera is at origin in view space, so it's negative of position)
+    viewDirVS = -P.xyz;
+    
+    gl_Position = projectionMatrix * P;
 }
--- cmake-build-debug/assets/shaders/fragment.glsl
+++ cmake-build-debug/assets/shaders/fragment.glsl
@@ -2,17 +2,29 @@
 
 in vec3 colorVS;
 in vec3 normalVS;
+in vec3 lightDirVS;
+in vec3 viewDirVS;
 
-uniform vec3 lightPosition;
 uniform vec3 lightColor;
 
-//fragment shader output
 out vec4 color;
 
-
 void main(){
-
-    color = vec4(colorVS.x, colorVS.y, colorVS.z, 1.0);
-
+    // Normalize necessary vectors
+    vec3 N = normalize(normalVS);
+    vec3 L = normalize(lightDirVS);
+    vec3 V = normalize(viewDirVS);
+    
+    // Diffuse component
+    float cosA = max(0.0, dot(N, L));
+    vec3 diffuseTerm = colorVS * lightColor * cosA;
+    
+    // Specular component
+    vec3 R = normalize(reflect(-L, N));
+    float cosBeta = max(0.0, dot(R, V));
+    float cosBetaShininess = pow(cosBeta, 32.0);  // 32.0 is shininess factor
+    vec3 specularTerm = vec3(1.0) * lightColor * cosBetaShininess;  // white specular highlights
+    
+    color = vec4(diffuseTerm + specularTerm, 1.0);
 }
 
